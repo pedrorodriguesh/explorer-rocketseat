8375a281adcc1e617b5b187122933f3ebba24acc
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-14 20:10:48 -0300

    array

---
+++ javascript/array.js
@@ -0,0 +1,14 @@
+// Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat'
+]
+
+console.log(animals[2])
+
+animals.push('Dog')
+animals.push('Lion')
+
+console.log(animals[3], animals[4])
