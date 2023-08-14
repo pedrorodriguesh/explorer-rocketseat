639f7bf82e331c55475bd9eab12ba65c3f0907eb
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-14 20:02:34 -0300

    js

---
+++ javascript/variavel.js
@@ -0,0 +1,10 @@
+let nota1 = 7
+let nota2 = 3
+let media = (nota1 + nota2) / 2
+
+console.log(`A média alcançada pelo aluno é ${media}`)
+    if (media >= 6){
+        console.log('Aluno aprovado')
+    } else{
+        console.log('Aluno reprovado')
+    }
