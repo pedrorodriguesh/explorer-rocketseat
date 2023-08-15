ec9ed0a87939abf4a10e3e3c2681660022fb228b
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-14 22:54:31 -0300

    exercise js

---
+++ javascript/practice.js
@@ -0,0 +1,22 @@
+// 1. Declare uma variável de nome weight.
+
+let weight = 88
+
+// 2. Que tipo de dado é a variável acima?
+
+console.log(typeof weight)
+
+/* 3. Declare uma variável e atribua valores para cada um dos dados:
+  name: String
+  age: Number(inteiro)
+  stars: Number(float)
+  isSubscribed: Boolean
+
+*/
+
+let name = 'Pedro'
+let age = 24
+let stars = 4.8
+let isSubscribed = true
+
+console.log(`O ${name} tem ${age} anos`)
