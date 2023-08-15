27e79b7c67d711c906cdb24809af0b9c44879cc0
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-15 10:37:08 -0300

    rename

---
+++ js/prototye.js
@@ -0,0 +1,17 @@
+// type conversetion (typecasting)
+// type coersion
+
+// Alteração de um tipo de dado para outro tipo
+
+
+console.log(Number('9') + 5) // aqui o Number tá transformando a string '9' em um number.
+
+
+
+
+// Manipulando strings e números
+
+let string = "123"
+console.log(Number(string))
+let number = 321
+console.log(String(number))
