e63a3aa9f903fc04b9b3d69bbcd84a472e7750a2
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-15 10:18:48 -0300

    function constructor

--- javascript/function8.js
+++ javascript/function8.js
@@ -1,1 +1,15 @@
-// 
+// function constructor
+
+
+function Person(name){
+    this.name = name
+    this.walk = function(){
+        return "andando"
+    }
+}
+
+const pedro = new Person("Pedro")
+const joao = new Person("Joao")
+
+console.log(pedro.walk())
+console.log(joao.walk())
