75256cb56b2fd4090b2054c7c3828277492ab20b
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-15 19:20:12 -0300

    js exercise

---
+++ javascript/exercicio02js/script.js
@@ -0,0 +1,11 @@
+/*
+    Solicite 2 números, faça a soma deles
+    e apresente o resultado final ao usuário.
+*/
+
+alert("Insira 2 números:")
+let numberOne = prompt("Digite o primeiro número")
+let numberTwo = prompt("Digite o segundo número")
+let sum = Number(numberOne) + Number(numberTwo)
+
+alert(`A soma entre os 2 números é ${sum}`)
