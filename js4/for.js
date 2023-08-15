dd4a12beeb1a732e6c536b8011fd42aa65757946
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-15 15:19:33 -0300

    loop js

---
+++ js4/for.js
@@ -0,0 +1,6 @@
+// estruturas de repetição
+// for
+
+for ( let i = 100; i > 0; i--){
+    console.log(`O valor é ${i}`)
+}
---
+++ js4/forof.js
@@ -0,0 +1,8 @@
+// for... of
+
+let name = 'Pedro'
+let names = ['Joao', 'Jorge', 'Lucas']
+
+for (let name of names) {
+    console.log(name)
+}
