7619793aa9c79560d3c18f11073b4f3175473d07
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-15 20:55:20 -0300

    exercise js

---
+++ javascript/exercicio04js/script.js
@@ -0,0 +1,15 @@
+/*
+    Solicitar o nome do aluno e as 3 notas
+    do bimestre calcular a média daquele aluno.
+
+    A média positiva deverá ser maior que 6
+
+    Se o aluno passou no bimestre, dar os 
+    parabéns.
+
+    Se o aluno não passou no bimestre, 
+    motivar o aluno a dar seu melhor na prova
+    de recuperação.
+
+    Em ambos os casos, mostre uma mensagem com o nome do aluno e a nota
+*/ 
