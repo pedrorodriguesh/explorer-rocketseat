76207ca8cec232a45d2e2104603fb30326719995
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-15 14:23:55 -0300

    js

---
+++ js2/ternario.js
@@ -0,0 +1,17 @@
+// É um operador que possui 3 pontos. Dependendo da condição, nós receberemos valores diferentes.
+
+
+// Condição então valor 1 se não valor 2
+// condition ? value1 : value2
+
+
+// Exemplos:
+
+
+// Posso dirigir?
+
+let age = 22
+
+const canIDrive = age >= 18 ? "Posso dirigir" : "Não posso dirigir"
+
+console.log(canIDrive)
