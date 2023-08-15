ca71679f3dab5e51d856af47374a756b2889d832
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-14 23:17:26 -0300

    calling function

---
+++ javascript/functions.js
@@ -0,0 +1,13 @@
+// criar um aplicativo de frases motivacionais
+
+//function statement
+function createPhrases(){
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+//execute, run, call, invoke
+createPhrases()
+console.log('---------')
+console.log('Fim do programa')
