8eb67e2ff9006c247c54caeca3431b254e80cbc3
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-15 10:58:00 -0300

    prototype5

--- js/prototype4.js
+++ js/prototype4.js
@@ -1,3 +1,13 @@
 // Criar um array com construtor
 let myArray2 = new Array('a', 'b', 'c')
 console.log(myArray2)
+
+
+//Contar elementos de um array
+console.log(['a', 'b', 'c'].length)
+
+
+//Transformar uma cadeia de caracteres em elementos de um array
+
+let word = 'manipulação'
+console.log(Array.from(word))
