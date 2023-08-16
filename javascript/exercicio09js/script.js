249db80ce3709eff83dfeb02cdcdabaf76f5aaba
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-16 19:07:06 -0300

    exercise 09

--- javascript/exercicio09js/script.js
+++ javascript/exercicio09js/script.js
@@ -9,3 +9,40 @@
 */
 
 /* peso / (altura * altura) */
+
+const patients = [
+    {
+        name: "Marcelo",
+        age: 25,
+        weight: 81,
+        height: 175,
+    },
+    {
+        name: "Pedro",
+        age: 24,
+        weight: 80,
+        height: 180,
+    },
+    {
+        name: "Paulo",
+        age: 27,
+        weight: 90,
+        height: 176,
+    },
+]
+
+
+function IMC(weight, height){
+    return (weight / ((height / 100) ** 2)).toFixed(2)
+}
+
+function printPacientIMC(patient){
+    return `
+        Paciente ${patient.name} possui o IMC de ${IMC(patient.weight, patient.height)}    
+    `
+}
+
+for (let patient of patients) {
+    let IMCmessage = printPacientIMC(patient)
+    alert(IMCmessage)
+}
