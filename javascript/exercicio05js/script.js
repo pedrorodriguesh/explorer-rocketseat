c8748a35f5a742fe199af249ad4f8b3669cc2a12
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-16 10:02:27 -0300

    market list

---
+++ javascript/exercicio05js/script.js
@@ -0,0 +1,20 @@
+/*
+  Capture 10 items para compor a lista de um supermercado.
+  
+  Após capturar os 10 items, imprima-os, separando por vírgula.
+*/
+
+let items = [] // setei um array vazio.
+
+for(let item = 0; item <= 5; item++){ // laço de repetição for (valor inicial ; condição de repetição ; incremento)
+
+    itemName = prompt("Digite o a mercadoria do mercado:")
+
+    items[item] = itemName
+}
+
+alert(items)
+
+
+
+
