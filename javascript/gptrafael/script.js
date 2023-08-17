bbbca3489cbf7c24071281c2824a46dfee9883e8
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-17 14:11:48 -0300

    chat gpt test

---
+++ javascript/gptrafael/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('myButton');
+const output = document.getElementById('output');
+
+button.addEventListener('click', function() {
+  output.innerText = 'Rafael Pereira';
+});
