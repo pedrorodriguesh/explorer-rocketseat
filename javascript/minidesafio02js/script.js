2270ee31bfbe5b34e9256f2db047e56661ba0116
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-16 23:20:48 -0300

    mini desafio 02

---
+++ javascript/minidesafio02js/script.js
@@ -0,0 +1,18 @@
+const students = [
+    {
+        name: "Buzzo",
+        grade1: 7,
+        grade2: 2,
+    },
+    {
+        name: "Pedro",
+        grade1: 9,
+        grade2: 5,
+    },
+    {
+        name: "Rafael",
+        grade1: 10,
+        grade2: 6,
+    },
+]
+
