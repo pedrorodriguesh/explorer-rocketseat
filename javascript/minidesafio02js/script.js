1d833455ab36c8ef35d5fa8a6e5fae8a33b7f0c3
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-16 23:48:06 -0300

    mini desafio 2 done

--- javascript/minidesafio02js/script.js
+++ javascript/minidesafio02js/script.js
@@ -16,3 +16,16 @@ const students = [
     },
 ]
 
+function media(grade1, grade2) {
+    let media = (grade1 + grade2) / 2
+    return media
+}
+
+for(let student of students){
+    mediaIndividual = media(student.grade1, student.grade2)
+    let situation = mediaIndividual < 7 ? 'Infelizmente não foi dessa vez. Estude mais' : 'Parabéns pela sua aprovação'
+
+    alert(`A média do ${student.name} é ${mediaIndividual}
+    ${situation}
+    `)
+}
