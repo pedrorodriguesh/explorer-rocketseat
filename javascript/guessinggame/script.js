5ffbfa07e86943a5e9e529e6b58d7ecd1ea52bb1
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-18 13:24:03 -0300

    more comments

--- javascript/guessinggame/script.js
+++ javascript/guessinggame/script.js
@@ -5,16 +5,16 @@ const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")  // aqui foi a criação de 2 variáveis para guardar a seleção do html pois usaria várias vezes.
 
 //callback function
-function handleTryClick(event){
+function handleTryClick(event){  // função tranquila, vai executar o bloco de código abaixo quando for chamada com o click no html.
     event.preventDefault()
 
-    const inputNumber = document.querySelector("#inputNumber")
+    const inputNumber = document.querySelector("#inputNumber") // aqui estou pegando o input do html.
 
-    if(Number(inputNumber.value) == randomNumber) {
+    if(Number(inputNumber.value) == randomNumber) { // além de pegar o input, preciso usar o . para acessar o value dele.
         screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        screen2.classList.remove("hide") // aqui com o .classList eu acesso as classes de uma determinada tag HTML, então adiciono ou removo a classe quando a função é ativada.
 
-        document.querySelector(".screen2 h2").innerText = `acertou em ${xAttempts} tentativas`
+        document.querySelector(".screen2 h2").innerText = `acertou em ${xAttempts} tentativas` // aqui o .innerText = altera o conteúdo do <h2> do HTML.
 
     }
     inputNumber.value = ""
