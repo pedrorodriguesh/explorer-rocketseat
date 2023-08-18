47931c4d7fe082bd68984b98c64b8594f63a9365
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-18 13:21:40 -0300

    comments

--- javascript/guessinggame/script.js
+++ javascript/guessinggame/script.js
@@ -1,8 +1,8 @@
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
+const randomNumber = Math.round(Math.random() * 10)  // função para gerar o número aleatório. O Math.round arredondo o número e o Math.random gera número aleatório entre 0 e 1, por isso o * 10 ali, para aumentar para  entre 0 e 10.
+let xAttempts = 1 // variável de controle de número de tentativas.
 
 const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
+const screen2 = document.querySelector(".screen2")  // aqui foi a criação de 2 variáveis para guardar a seleção do html pois usaria várias vezes.
 
 //callback function
 function handleTryClick(event){
@@ -29,7 +29,6 @@ const again = document.querySelector("#again")
 
 
 tryy.addEventListener('click', handleTryClick)
-
 again.addEventListener('click', function(){
 
     screen1.classList.remove("hide")
