e71500492474bf4ce4771db501a69f3c2522cc81
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-21 00:06:17 -0300

    js

--- javascript/imc/script.js
+++ javascript/imc/script.js
@@ -1,8 +1,21 @@
-function calculate(){
+const popup = document.querySelector('.modal-wrapper')
+
+function calculate(event){
+
+    event.preventDefault()
 
     let weight = document.querySelector('#weight')
-    let 
+    let cm = document.querySelector('#cm')
+    let height = Number(cm.value / 100).toFixed(2)
+    let imc = Number(weight.value / (height*height)).toFixed(2)
 
+    popup.classList.add("open")
+
+    document.querySelector(".modal-wrapper h2").innerText = `Seu IMC é de ${imc}`
+}
+
+function back(){
+    popup.classList.remove("open")
 }
 
 
