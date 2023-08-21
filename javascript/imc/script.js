99cbc17d1ee21e15cd105272a8c339fc6326dee3
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-20 21:31:42 -0300

    trying to do js

---
+++ javascript/imc/script.js
@@ -0,0 +1,12 @@
+let weight = document.querySelector('#weight')
+let cm = document.querySelector('#height')
+
+cm = Number(height)
+weight = Number(height)
+
+let height = cm / 100 
+
+alert(height + weight)
+
+
+
