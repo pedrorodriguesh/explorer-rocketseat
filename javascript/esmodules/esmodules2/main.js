17e6149a5a57eafd44c018b62ac139762279ab59
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-21 15:06:48 -0300

    export and import default

---
+++ javascript/esmodules/esmodules2/main.js
@@ -0,0 +1,6 @@
+import qualquerNome from './util.js' // aqui eu import com QUALQUER NOME que eu queira. Utilizando então "import (nomequeeuquero) from (outro arquivo .js)"
+
+alert(qualquerNome) // aqui eu estou usando o nome que coloquei ali em cima, e ta me mostrando a variável que tá no outro arquivo JS.
+
+
+// Eu só posso ter uma exportação default por arquivo.
---
+++ javascript/esmodules/esmodules2/util.js
@@ -0,0 +1,5 @@
+const message = "mensagem" // aqui defini a variável com a string "mensagem"
+
+export default message  // aqui eu estou exportando de maneira default a minha variável (message)
+
+// posso também exportar um dado direto, sem inserir ele dentro de uma variável, por exemplo uma String com: "export default "algum dado" "
