7093dc14116b06675c5b0ffc0d6b7c0e8256ee91
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-21 09:22:19 -0300

    js completed

--- javascript/imc/script.js
+++ javascript/imc/script.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector('.modal-wrapper') // aqui eu to trasnformando em variável só pra facilitar ali na frente, não é necessário 
+const error = document.querySelector('.alert-error')
 
-function calculate(event){ 
-
+function calculate(event){
     event.preventDefault() // aqui é pra previnir que o form do HTML recarregue a página
 
     let weight = document.querySelector('#weight') // pegando o peso do input
@@ -9,9 +9,17 @@ function calculate(event){
     let height = Number(cm.value / 100).toFixed(2) // transformando a altura em cm em altura em metros.
     let imc = Number(weight.value / (height*height)).toFixed(2) // cálculo do IMC, sempre usando o .value pra pegar o valor do input.
 
-    popup.classList.add("open") // adicionando a class open ao popup
+    if(isNaN(imc)){
+        error.classList.add('open')
+    } else{
 
-    document.querySelector(".modal-wrapper h2").innerText = `Seu IMC é de ${imc}` // mudando o conteúdo do h2 do popup.
+        error.classList.remove('open')
+        
+        popup.classList.add("open") // adicionando a class open ao popup
+    
+        document.querySelector(".modal-wrapper h2").innerText = `Seu IMC é de ${imc}` // mudando o conteúdo do h2 do popup.
+    }
+
 }
 
 function back(){
