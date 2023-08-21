df958704e270e2d4cb7c885dd3d73b1ae0531f62
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-20 22:38:55 -0300

    js

--- javascript/imc/script.js
+++ javascript/imc/script.js
@@ -1,13 +1,14 @@
 let weight = document.querySelector('#weight')
-let cm = document.querySelector('#height')
+var cm = document.querySelector('#height')
 
 cm = Number(cm)
 weight = Number(weight)
 
-let height = (cm / 100).toFixed(2)
+let height = Number(cm / 100).toFixed(2)
+height = Number(height)
 
 function calculate(){
-    alert(`Seu IMC é`)
+    alert(`Seu IMC é ${cm.value}`)
 }
 
 
