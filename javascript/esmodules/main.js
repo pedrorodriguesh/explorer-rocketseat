3af121dbf2c3ab8e3488c776774a4cf3a5bd0bc9
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-21 14:54:36 -0300

    studying es modules

--- javascript/esmodules/main.js
+++
@@ -1,3 +0,0 @@
-import message from './util.js' // nessa linha ele importa e já executa o arquivo.
-
-console.log(message)
--- javascript/esmodules/util.js
+++
@@ -1,3 +0,0 @@
-const message = "mensagem" // variável tá isolada nesse arquivo, não da pra usar ela em outro lugar
-
-export default message // aqui a gente exporta a variável pra outro arquivo, o que normalmente não aconteceria por conta do escopo.
