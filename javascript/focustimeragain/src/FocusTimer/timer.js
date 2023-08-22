01015a6bcdb882bfb06c261d85121b493bb46787
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-22 12:45:29 -0300

    timer working

--- javascript/focustimeragain/src/FocusTimer/timer.js
+++ javascript/focustimeragain/src/FocusTimer/timer.js
@@ -1,5 +1,31 @@
 import state from "./state.js"
 import * as el from "./elements.js"
+import { reset } from "./actions.js"
+
+export function countdown(){
+    if(!state.isRunning){
+        return
+    }
+
+    let minutes = Number(el.minutes.textContent)
+    let seconds = Number(el.seconds.textContent)
+
+    seconds--
+
+    if(seconds < 0){
+        seconds = 59
+        minutes--
+    }
+
+    if(minutes <0){
+        reset()
+        return
+    }
+
+    updateDisplay(minutes, seconds)
+    
+    setTimeout(() => countdown(), 1000) // 1000ms > 1s.
+} // função que recebe outra função pra ser executada depois de um tempo é a callback function.
 
 export function updateDisplay(minutes, seconds){
     minutes = minutes ?? state.minutes
@@ -7,7 +33,4 @@ export function updateDisplay(minutes, seconds){
 
     el.minutes.textContent = String(minutes).padStart(2, "0")
     el.seconds.textContent = String(seconds).padStart(2, "0")
-
-
-
 }
