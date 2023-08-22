25f00c1025fb8a653bce82003d728763a9b82e96
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-21 23:05:44 -0300

    focus timer done

---
+++ javascript/focustimer/js/FocusTimer/sounds.js
@@ -0,0 +1,8 @@
+export const buttonPressAudio = new Audio('./assets/assets_button-press.wav')
+
+export const kitchenTimer = new Audio ('./assets/assets_kichen-timer.mp3')
+
+export const bgAudio = new Audio ('./assets/assets_bg-audio.mp3')
+
+bgAudio.loop = true
+
