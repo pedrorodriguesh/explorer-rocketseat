1e64101d307fc9c83832319e4251591615250c94
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-22 20:46:30 -0300

    dying

--- javascript/focustimer2/src/focustimer/actions.js
+++ javascript/focustimer2/src/focustimer/actions.js
@@ -2,17 +2,30 @@ import * as el from "./elements.js"
 import state from "./state.js"
 import * as timer from './timer.js'
 
+//adicionando 5 minutos.
 export let minutes = Number(el.minutes.textContent)
 const plus = document.getElementById('plus')
-
 plus.addEventListener('click', () => {
     minutes = minutes + 5
     
     if(minutes > 60){
-        return
+        minutes = 60
     }
 
-    el.minutes.textContent = minutes
+    
+    timer.updateDisplay(minutes)
+})
+
+//removendo 5 minutos.
+const minus = document.getElementById('minus')
+minus.addEventListener('click', () => {
+    minutes = minutes - 5
+
+    if(minutes < 0){
+        minutes = 0
+    }
+
+
     timer.updateDisplay(minutes)
 })
 
