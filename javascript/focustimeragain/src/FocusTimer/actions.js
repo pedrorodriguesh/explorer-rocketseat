949d44d451897fb21d7508cfe2ebf1062da09377
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-22 10:46:34 -0300

    starting actions

---
+++ javascript/focustimeragain/src/FocusTimer/actions.js
@@ -0,0 +1,7 @@
+export function toggleRunning(){
+    
+}
+
+export function set(){
+    
+}
--- javascript/focustimeragain/src/FocusTimer/events.js
+++ javascript/focustimeragain/src/FocusTimer/events.js
@@ -1,12 +1,13 @@
 import { controls } from "./elements.js"; // estou recebendo a section controls como uma variável, vinda do elements.js.
+import * as actions from "./actions.js"
 
 export function registerControls (){
     controls.addEventListener('click', (event) =>{ // evento de click, mesma coisa que la no HTML.
         const action = event.target.dataset.action
-        if(action === undefined ) {
+        if(typeof actions[action] != "function") {
             return
         }
 
-        console.log(action)
+        actions[action]()
     })
 }
