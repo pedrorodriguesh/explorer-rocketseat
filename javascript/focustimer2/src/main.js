77cc88359af4eedfccb6db070e99695d8dfb1908
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-22 21:21:34 -0300

    almost done

--- javascript/focustimer2/src/main.js
+++ javascript/focustimer2/src/main.js
@@ -12,15 +12,26 @@ const forestBtn = document.getElementById('tree')
 forestBtn.addEventListener('click', () => {
     sounds.forestSound.play()
     sounds.rainSound.pause()
+    sounds.fireSound.pause()
     sounds.forestSound.loop = true
 })
 
 const rainBtn = document.getElementById('rain')
 rainBtn.addEventListener('click', () => {
+    rainBtn.classList.toggle('selected')
     sounds.rainSound.play()
     sounds.forestSound.pause()
+    sounds.fireSound.pause()
     sounds.rainSound.loop = true
 
+})
+
+const fireBtn = document.getElementById('fire')
+fireBtn.addEventListener('click', () => {
+    sounds.fireSound.play()
+    sounds.fireSound.loop = true
+    sounds.rainSound.pause()
+    sounds.forestSound.pause()
 })
 
 
