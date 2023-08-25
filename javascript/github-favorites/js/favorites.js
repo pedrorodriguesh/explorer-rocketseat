9ca598376c58e1e9a008106307e70300e2d7390f
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-08-25 14:46:50 -0300

    logica para adicionar

--- javascript/github-favorites/js/favorites.js
+++ javascript/github-favorites/js/favorites.js
@@ -1,32 +1,40 @@
+export class GithubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}` // endpoint é onde eu vou buscar os dados.
+
+        return fetch(endpoint)      // o fetch() vai me devolver os dados do endpoit acima, então (then) vai transformar esses dados em json e eu estruturo os dados abaixo.
+        .then(data => data.json()) // data é o nome que eu coloquei pro conjunto de dados que vou receber da api, ai data.json() transforma em json.
+        .then(({ login, name, public_repos, followers }) => ({
+            login,
+            name,                     // aqui eu usei a desestruturação, peguei apenas os itens que eu quero e separei em um novo objeto.
+            public_repos,
+            followers
+        }))
+    }
+}
+
+
 // classe que vai conter a lógica dos dados. Como os dados serão estruturados.
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root) // aqui o this root tá pegando a div #app lá do HTML, quem trouxe a div pra cá foi o "super" da classe debaixo com a herança.
         this.load()
+
+        GithubUser.search('pedrorodriguesh')
+        .then(user => console.log(user))
     }
 
     load() { // essa função load é responsável por carregar e criar os dados, quando chegar a hora de mostar isso lá embaixo eles já estão criados aqui em cima.
-        this.entries = [ // criei um array com objetos, cada objeto vai guardar os dados da conta.
-        {
-            login: 'pedrorodriguesh',
-            name: 'Pedro Henrique Rodrigues',
-            public_repos: '11',
-            followers: '1'
-        },
-        {
-            login: 'ZehCoque',
-            name: 'José Coque',
-            public_repos: '29',
-            followers: '6'
-        }
-       ]
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+
     }
 
     delete(user) { 
         //High-order functions (map, filter, reduce)
         const filteredEntries = this.entries.filter(entry => entry.login !== user.login) // pelo principio da imutabilidade, eu não to mexendo no array [this.entries] o filter me retorna um NOVO array, o [filteredEntries] 
 
-        console.log(filteredEntries)
+        this.entries = filteredEntries
+        this.update()
     }
 }
 
@@ -39,6 +47,19 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody') // this.root = div#app do HTML, estou pegando dentro dela, o Tbody que está dentro da Table.
 
         this.update() // aqui estamos tendo uma corrente de funções, estou chamando a update(), que na update() chama a removeAllTr()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+
+            const { value } = this.root.querySelector('.search input') // de novo usando a desestruturação, eu peguei o input e usando { value } peguei de dentro dele direto.
+
+            console.log(value)
+           
+            
+        }
     }
 
     update() { // função update. 
